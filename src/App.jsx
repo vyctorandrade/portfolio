9e6ec06dd3c603d63c8158690e9b6fcd73ed242e
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-10-25 16:27:07 -0300

    coles

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './pages/Home/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 
 function App() {
  
 
   return (
     <>
+
+     {/* Transferir para rotas os links
+    <Router>
+
+    <Routes>
+    <Route exact path='/' element = {<Home/>}></Route>
+    <Route exact path='/'> Sobre </Route>
+    <Route exact path='/'> Projetos </Route>
+    <Route exact path='/'> Contatos </Route>
+    </Routes>
+    </Router> */}
+
+   
      <Header/>
      <Home/>
      <Footer/>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
 import styles from './Header.module.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+
 
 function Header() {
   return (
    <header className={styles.header}>
         <span>vyAndrade.dev</span>
         
-        {/* Transferir para rotas os links */}
-        <Router>
-
-            <Routes>
-                <Route exact path='/'> Home </Route>
-                <Route exact path='/'> Sobre </Route>
-                <Route exact path='/'> Projetos </Route>
-                <Route exact path='/'> Contatos </Route>
-            </Routes>
-        </Router>
+        
         <div>
         <a href="#">Home</a>
         <a href="#">Sobre</a>
