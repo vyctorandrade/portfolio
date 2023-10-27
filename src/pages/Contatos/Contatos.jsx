82309b5bfedbba183506440b97b6d6fca72b6406
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-10-27 09:30:27 -0300

    up

--- src/pages/Contatos/Contatos.jsx
+++ src/pages/Contatos/Contatos.jsx
@@ -1,6 +1,9 @@
 
 import Footer from '../../components/Footer/Footer'
 import Container from '../../components/Container/Container'
+import styles from './Contatos.module.css'
+import { GoMail } from 'react-icons/go'
+import { BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs'
 
 
 function Contatos() {
@@ -9,7 +12,31 @@ function Contatos() {
     
    
     <Container>
-    <h2>Contatos</h2>
+    <section className={styles.contatos}>
+      <h2>Contatos</h2>
+        <h3>Entre em contato</h3>
+        <p>Para que possamos conversar mais sobre</p>
+
+        <div className={styles.icones}>
+
+        <a href='mailto:vyctoravaleriano@icloud.com' target='_blank' rel='noopener noreferrer'>
+        <GoMail className={styles.icone}/> 
+        </a>
+        
+        <a href='https://www.instagram.com/_vyctorandrade' target='_blank' rel='noopener noreferrer'>
+        <BsInstagram className={styles.icone}/>
+        </a>
+
+        <a href='https://github.com/vyctorandrade' target='_blank' rel='noopener noreferrer'>
+        <BsGithub className={styles.icone}/>
+        </a>
+
+        
+        <a href='http://www.linkedin.com/in/devvyctorandrade' target='_blank' rel='noopener noreferrer'>
+        <BsLinkedin className={styles.icone}/>
+        </a>
+        </div>
+    </section>
     </Container>
     <Footer/>
     </>
