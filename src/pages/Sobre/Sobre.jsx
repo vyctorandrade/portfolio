262a4bdef6cbd2a4ee3d6ad587835ea1b7c5be63
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-10-30 11:52:46 -0300

    update

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,11 +1,53 @@
+import styles from './Sobre.module.css'
+import avatar from './images/avatar.png'
+import html from './images/icon-html.svg'
+import css from './images/icon-css.svg'
+import js from './images/icon-js.svg'
+import react from './images/icon-react.svg'
+import node from './images/icon-node.svg'
+import sql from './images/icon-sql.svg'
+
+
 
 function Sobre() {
   return (
-    <>
+    <section className={styles.sobre}>
 
-    <h2>Sobre</h2>
+    <div className={styles.bio}>
+        <img src={avatar} alt="Avatar" className={styles.avatar} />
+        <div className={styles.textos}>
+          <h2>Sobre</h2>
+          <p>
+            Sou <span>Vyctor Andrade</span> <br />
+            <strong>Dev Full Stack</strong>
+          </p>
 
-    </>
+          <p>Graduado em Sistemas de Informação.</p>
+
+          <p>Sou apaixonado por transformar ideias em realidade digital.</p>
+
+          <p>
+            Especializado em criação de aplicações dinâmicas e intuitivas, 
+            <br />
+
+            com foco na experiência do usuário.
+          </p>
+        </div>
+    </div>
+   
+    <div className={styles.techs}>
+      <h3>Techs</h3>
+      <div className={styles.icones}>
+      <img src={html} alt="Ícone do html" />
+      <img src={css} alt="Ícone do css" />
+      <img src={js} alt="Ícone do js" />
+      <img src={react} alt="Ícone do react" />
+      <img src={node} alt="Ícone do node" />
+      <img src={sql} alt="Ícone do sql" />
+      </div>
+    </div>
+
+    </section>
   )
 }
 
