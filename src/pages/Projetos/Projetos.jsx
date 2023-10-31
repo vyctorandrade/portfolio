aa3d768621b501604215d66e287345b29079fb20
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-10-31 10:42:09 -0300

    .

--- src/pages/Projetos/Projetos.jsx
+++ src/pages/Projetos/Projetos.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react"
+import Card from "../../components/Card/Card"
+import styles from './Projetos.module.css'
 
 
 function Projetos() {
+
+  const [ repositorios, setRepositorios] = useState([])
+
+  
+
   return (
-    <>
+    <section className={StyleSheet.projetos} >
  
     <h2>Projetos</h2>
+    <section className={styles.lista}>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    </section>
    
-    </>
+    </section>
   )
 }
 
