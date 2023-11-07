31b8ba8bb1fbb9304667aa74625d3f149c5840ae
Vyctor Andrade <134069813+vyctorandrade@users.noreply.github.com>
2023-11-07 12:38:18 -0300

    menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,21 @@
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom'
 import logo1 from './images/logo1.png'
+import { useState } from 'react'
 
 
 
 function NavBar() {
+  
+
+  // Estado de exebição
+  const [showMenu, setShowMenu ] = useState (false)
+  // Função que vai trocar o estado
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+  
+  
   return (
     <>
    <header className={styles.header}>
@@ -14,12 +25,23 @@ function NavBar() {
         <img src={logo1} alt="Ícone da logo" />
         </Link>
         
-        <nav>
+        <nav 
+        className={`${styles.menuSandwich} ${ showMenu ? styles.show : '' }`}
+        >
         <Link to='/'>Home</Link>
         <Link to='sobre'>Sobre</Link>
         <Link to='projetos'>Projetos</Link>
         <Link to='contatos'>Contatos</Link>
         </nav>
+
+        <div
+           className={styles.menuButton}
+           onClick={toggleMenu}      
+        >   
+          <span className={styles.linha}></span>
+          <span className={styles.linha}></span>
+          <span className={styles.linha}></span>
+        </div>
         
       
    </header>
